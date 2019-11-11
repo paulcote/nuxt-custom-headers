ea00f31f4a1f484f2b8434da8463f398797c5972
Paul Côté <paul.cote@libeo.com>
2019-11-11 15:45:08 -0500

    feat: get httpHeaders from layouts

--- middleware.js
+++ middleware.js
@@ -1,18 +1,43 @@
 /* eslint-disable import/no-unresolved, import/no-extraneous-dependencies */
-import middleware from '@@/.nuxt/middleware'
+import middleware from "@@/.nuxt/middleware";
 
-middleware['nuxt-custom-headers'] = context => {
+middleware["nuxt-custom-headers"] = async context => {
   if (process.server) {
-    const headers = [].concat.apply([], (context.route.matched.map(({ components }) => Object.values(components)))).reduce((headers, component) => {
-      if (component.options && component.options[context.env.NUXT_CUSTOM_HEADERS_FUNCTION]) {
-        Object.assign(headers, component.options[context.env.NUXT_CUSTOM_HEADERS_FUNCTION](context))
-      }
+    const headers = await [].concat
+      .apply(
+        [],
+        context.route.matched.map(({ components }) => Object.values(components))
+      )
+      .reduce(async (previousPromise, component) => {
+        const headers = await previousPromise;
+        const layoutName = component.options.layout;
+        const layoutComponent = await context.app.methods.loadLayout(
+          layoutName
+        );
+        if (
+          layoutComponent &&
+          layoutComponent[context.env.NUXT_CUSTOM_HEADERS_FUNCTION]
+        ) {
+          Object.assign(
+            headers,
+            layoutComponent[context.env.NUXT_CUSTOM_HEADERS_FUNCTION](context)
+          );
+        }
+        if (
+          component.options &&
+          component.options[context.env.NUXT_CUSTOM_HEADERS_FUNCTION]
+        ) {
+          Object.assign(
+            headers,
+            component.options[context.env.NUXT_CUSTOM_HEADERS_FUNCTION](context)
+          );
+        }
 
-      return headers
-    }, {})
+        return headers;
+      }, {});
 
     Object.keys(headers).map(function(header) {
-      context.res.setHeader(header, headers[header])
+      context.res.setHeader(header, headers[header]);
     });
   }
-}
+};
